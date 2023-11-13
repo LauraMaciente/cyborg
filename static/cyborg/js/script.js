0d683cc1a797826ccd1eb424f471d9f856d1279a
Laura Freitas Maciente <149081458+LauraMaciente@users.noreply.github.com>
2023-11-13 12:19:59 -0300

    Update script.js

--- static/cyborg/js/script.js
+++ static/cyborg/js/script.js
@@ -2,10 +2,10 @@ function menuShow(){
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')){
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "menu.png";
+        //document.querySelector('.icon').src = "../images/menu.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "close.png";
+        //document.querySelector('.icon').src = "../images/close.png";
     }
 }
 
